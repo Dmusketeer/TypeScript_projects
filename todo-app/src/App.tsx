f206b210d96ebd49f63540d32d31a7bdf316fceb
Dmuskeeter <tdheeraj9a@gmail.com>
2023-11-17 22:34:23 +0530

    create functions and passed as props

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -2,13 +2,34 @@ import React, {useState } from 'react'
 import './App.css'
 import { TodoTypes } from './types/Todo'
 import AddTodo from "./components/AddTodo"
-import TodoLists from "./components/TodoLists"
+import TodoList from "./components/TodoLists"
 
 const App:React.FC =()=>{
+  const [todos,setTodos]=useState<TodoTypes[]>([])
+  
+  const addTodo=(text:string)=>{
+    setTodos((prevTodos)=>{
+   return [  ...prevTodos,{id:prevTodos.length+1,text,completed:false}]
+    })
+  }
+
+  const toggleTodo=(id:number)=>{
+    setTodos((prevTodos)=>
+    prevTodos.map((todo)=>
+    todo.id===id ?{...todo,completed:!todo.completed}:todo
+    )
+    )
+  }
+
+const deleteTodo = (id:number)=>{
+setTodos((prevTodos)=>prevTodos.filter((todo)=>todo.id!==id))
+}
+
   return (
     <div className='App'>
      <h1>Todo App</h1>
-      
+     <AddTodo addTodo={addTodo}/>
+     <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
     </div>
   )
 }
