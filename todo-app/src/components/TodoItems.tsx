c87ba37dd73d5b6fe50f22597060687942a6e717
Dmuskeeter <tdheeraj9a@gmail.com>
2023-11-17 23:32:06 +0530

    todo items component

---
+++ todo-app/src/components/TodoItems.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { TodoTypes } from '../types/Todo'
+
+interface Props{
+    todo:TodoTypes;
+    toggleTodo:(id:number)=> void;
+    deleteTodo:(id:number)=> void;
+}
+const TodoItems:React.FC<Props> = ({todo,toggleTodo,deleteTodo}) => {
+  return (
+    <div>
+        <input type="checkbox" checked={todo.completed} onChange={()=>toggleTodo(todo.id)} />
+        <span style={{textDecoration:todo.completed?"line-through":"none"}}>{
+            todo.text
+        }</span>
+        <button onClick={()=>deleteTodo(todo.id)}>Delete</button>
+    </div>
+  )
+}
+
+export default TodoItems
