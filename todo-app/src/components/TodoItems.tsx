267916a8ba38b777431b904efbef62e71739cde4
Dmuskeeter <tdheeraj9a@gmail.com>
2023-12-30 19:46:17 +0530

    typscript Todo

--- todo-app/src/components/TodoItems.tsx
+++ todo-app/src/components/TodoItems.tsx
@@ -9,11 +9,18 @@ interface Props{
 const TodoItems:React.FC<Props> = ({todo,toggleTodo,deleteTodo}) => {
   return (
     <div className='TodoItem'>
-        <input type="checkbox" checked={todo.completed} onChange={()=>toggleTodo(todo.id)} />
-        <span style={{textDecoration:todo.completed?"line-through":"none"}}>{
+        <input type="checkbox"  style={{ transform: 'scale(1.5)' }} checked={todo.completed} onChange={()=>toggleTodo(todo.id)} />
+        <span className='TodoItemText' style={{
+        cursor: 'pointer',
+        textDecoration: todo.completed ? 'line-through' : 'none',
+        fontSize: '26px',
+        color: todo.completed ? '#888' : '#333',
+        fontFamily: 'Arial, sans-serif',
+        fontWeight: todo.completed ? 'normal' : 'bold',  
+    }}>{
             todo.text
         }</span>
-        <button onClick={()=>deleteTodo(todo.id)}>Delete</button>
+        <button className='TodoItemButton' onClick={()=>deleteTodo(todo.id)}>DELETE</button>
     </div>
   )
 }
